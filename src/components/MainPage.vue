5eb631f4199f20b77f34f639188a3a3f84cc3554
Julian Fragoso <starsmember@yahoo.com>
2023-11-12 23:01:42 -0600

    main page get employee

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -1,22 +1,23 @@
 <script setup>
-    import MainSearch from '@/components/MainSearch.vue'
-    import MainCards from '@/components/MainCards.vue'
-    import MainPagination from '@/components/MainPagination.vue'
+import { onMounted } from 'vue'
+
+import MainSearch from '@/components/MainSearch.vue'
+import MainCards from '@/components/MainCards.vue'
+import MainPagination from '@/components/MainPagination.vue'
 </script>
 
 <template>
     <div class="wrapper">
-    <MainSearch /> 
-    <MainCards />
-    <MainPagination />
+        <MainSearch />
+        <MainCards />
+        <MainPagination />
 
 
     </div>
 </template>
 
 <style scoped lang="postcss">
-    .wrapper {
-        @apply container mx-auto flex flex-col gap-4 py-4;
-    }
-
+.wrapper {
+    @apply container mx-auto flex flex-col gap-4 py-4;
+}
 </style>
