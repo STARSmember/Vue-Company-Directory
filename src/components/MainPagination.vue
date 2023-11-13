b78655f2c3add9c2d8c67c673b4fe0aa89b3b3c6
Julian Fragoso <starsmember@yahoo.com>
2023-11-12 23:33:58 -0600

    added pagination api

--- src/components/MainPagination.vue
+++ src/components/MainPagination.vue
@@ -1,55 +1,51 @@
 <script setup>
-import { ref } from 'vue' 
-const pages = ref(10)
-const activePage = ref(4)
+import useAPI from '@/composables/useAPI'
 
-const prePage = () => {
+const { activePage, pages, getEmployees } = useAPI()
+
+const prevPage = async () => {
     if (activePage.value > 1) {
         activePage.value--
+        await getEmployees()
     }
 }
-
-const nextPage = () => {
+const nextPage = async () => {
     if (activePage.value < pages.value) {
         activePage.value++
+        await getEmployees()
     }
 }
-
+const jumpPage = async (page) => {
+    activePage.value = page
+    await getEmployees()
+}
 </script>
 
-
-
 <template>
-
-    <!-- This code below activates the page numbers at the bottom of the page.-->
-    <div class="pageination">
-         <button class="action" :disabled="activePage === 1" @click="prePage">Prev</button>
-            <button v-for="page in pages" class="page" v-bind:key="page" :class="page === activePage ? 'active' : ''" @click="activePage = page">
-                {{ page }}
-                </button>
-            <button class="action" :disabled="activePage === pages" @click="nextPage">Next</button>
-         
+    <div class="pagination">
+        <button class="action" :disabled="activePage === 1" @click="prevPage">Prev</button>
+        <button v-for="page in pages" :key="page" class="page" :class="page === activePage ? 'active' : ''"
+            @click="jumpPage(page)">
+            {{ page }}
+        </button>
+        <button class="action" :disabled="activePage === pages" @click="nextPage">Next</button>
     </div>
-    <!---------------------------------------------------------------------------------------------->
-    <!---------------------------------------------------------------------------------------------->
 </template>
 
-
-<!---------------- STYLING OF THE ICONS OF THE PAGE INATION   -------------------------------------->
 <style lang="postcss" scoped>
-    .pageination {
+.pagination {
+    @apply flex justify-center gap-4;
 
-        @apply flex justify-center gap-4;
-        .action {
-            @apply rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md hover:bg-blue-200 disabled:text-slate-200 hover:disabled:bg-slate-100;
-            
-        }
-        .page {
-            @apply rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md hover:bg-blue-200;
-            &.active {
-                @appply bg-yellow-700 text-slate-100 hover:bg-yellow-600;
-            }
+    .action {
+        @apply rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md hover:bg-slate-200 disabled:text-slate-400 hover:disabled:bg-slate-100;
+    }
+
+    .page {
+        @apply rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md hover:bg-slate-200;
+
+        &.active {
+            @apply bg-yellow-700 text-slate-100 hover:bg-yellow-600;
         }
     }
+}
 </style>
-<!---------------- STYLING OF THE ICONS OF THE PAGE INATION   -------------------------------------->
