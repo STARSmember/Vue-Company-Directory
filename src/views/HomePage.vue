3349bf280008c020f164d3ed81cda72390642b10
Julian Fragoso <starsmember@yahoo.com>
2023-12-11 16:01:25 -0600

    added home page.

---
+++ src/views/HomePage.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+const title = ref('Hello There ')  
+</script>
+
+<template>
+    <main class="felx min-h-screen items-center justify-center">
+        <h1 class="text-6xl font-thin-slate-800">{{ title }}</h1>
+    </main>
+
+</template>
